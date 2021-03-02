ddd840ba29340d08a9c7a94a1d6c0e3e7c3ab302
demondo <demondocs1.6@gmail.com>
2021-03-03 00:22:52 +0100

    initial config

---
+++ src/EfficientCursor.tsx
@@ -0,0 +1,9 @@
+interface EfficientCursorProps {
+  children: React.ReactChild;
+}
+
+const EfficientCursor = ({ children }: EfficientCursorProps) => {
+  return <div>{children}</div>;
+};
+
+export default EfficientCursor;
